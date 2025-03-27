54697179dae310e01f86b9d5d35b8efc1d6a710f
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-27 14:15:50 +0530

    helper function for status check

---
+++ src/lib/monitor.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+
+export const checkWebsiteStatus = async (url) => {
+    try {
+        const start = Date.now(); // current time ko store krne ke liye before making req.
+        const response = await axios.get(url, { timeout: 5000 }); // 5seconds ke liye
+        const end = Date.now(); // timestamps store krne ke liye jab response mil jayega
+
+
+        console.log(response);
+        return {
+            url, // jo url ko check kiya h
+            status: response.status, 
+            isUp: true,
+            responseTime: `${end - start}ms`,
+        }
+    } catch (error) {
+        // if req fails:
+        return {
+            url,
+            status: error.response?.status || "DOWN",
+            isUp: false,
+            responseTime: "N/A",
+        }
+    }
+}
