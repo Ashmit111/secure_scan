bf4c14fce1ae65f5faba05f8413209ff8e7576d5
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-27 15:18:19 +0530

    updated

---
+++ src/Models/website.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const WebsiteSchema = new mongoose.Schema(
+    {
+        url: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        status: {
+            type: String,
+            enum: ["UP", "DOWN"],
+            default: "UP"
+        },
+        responseTime: {
+            type: String,
+            default: "N/A"
+        },
+        lastChecked: {
+            type: Date,
+            default: Date.now
+        },
+        logs: [
+            {
+                timestamp: { type: Date, default: Date.now },
+                status: { type: String },
+                responseTime: { type: String },
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+export const Website = mongoose.models.Website || mongoose.model("Website", WebsiteSchema);
